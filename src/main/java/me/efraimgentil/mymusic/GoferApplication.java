0e8bc35be64df97f8fc666eb9df36c3740ae0b03
efraimgentil <efraim.gentil@gmail.com>
2017-03-08 21:10:05 -0300

    Initial commit

---
+++ src/main/java/me/efraimgentil/mymusic/GoferApplication.java
@@ -0,0 +1,12 @@
+package me.efraimgentil.mymusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoferApplication.class, args);
+	}
+}
---
+++ src/test/java/me/efraimgentil/mymusic/GoferApplicationTests.java
@@ -0,0 +1,16 @@
+package me.efraimgentil.mymusic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoferApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
